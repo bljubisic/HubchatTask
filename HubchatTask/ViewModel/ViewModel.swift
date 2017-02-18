71fa1ec62b78c960a69093e0bf3ad50579be5c70
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-02-18 23:30:21 +0100

    ViewModel implementation

--- HubchatTask/ViewModel/ViewModel.swift
+++ HubchatTask/ViewModel/ViewModel.swift
@@ -12,11 +12,15 @@ import UIKit
 class ViewModel: ViewModelProtocol{
     
     var posts: [PostData]?
+    var header: PhotographForumHeader?
     weak var delegate: ViewModelDelegate?
     var model: ModelProtocol?
     
     init(withModel model: ModelProtocol) {
+        self.header = PhotographForumHeader()
+        self.posts = []
         self.model = model
+        self.model?.delegate = self
     }
     
     func getPhotographyForumHeaderData() {
@@ -27,13 +31,27 @@ class ViewModel: ViewModelProtocol{
     }
     
     func setImageFrom(url: String, toImageView image: UIImageView) {
-        
+        model?.setImageWithCompletion(withURL: url, completion: { (data, error) in
+            guard let tmpData = data else {
+                return
+            }
+            let tmpImage = UIImage(data: tmpData)
+            
+            DispatchQueue.main.async {
+                image.image = tmpImage
+            }
+        })
     }
     
 }
 
 extension ViewModel: ModelDelegate {
     func photographyForumDataReceived(headerData: PhotographForumHeader) -> Void {
-        
+        guard let delegate = self.delegate else {
+            return
+        }
+        self.header = headerData
+        self.posts = headerData.posts
+        delegate.photographyForumDataReceived(photographyStruct: headerData)
     }
 }
