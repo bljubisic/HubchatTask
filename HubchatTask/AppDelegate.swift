4cdfc7460b9b42c2fcb482d81fbf4fd208cadfe8
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-02-18 23:30:51 +0100

    AppDelegate do not use storyboard anymore

--- HubchatTask/AppDelegate.swift
+++ HubchatTask/AppDelegate.swift
@@ -13,9 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    lazy var model: ModelProtocol = HCModel()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let photographyForumVC = PhotgraphyForumViewController()
+        photographyForumVC.viewModel = ViewModel(withModel: model)
+        window?.rootViewController = photographyForumVC
+        window?.makeKeyAndVisible()
         return true
     }
 
