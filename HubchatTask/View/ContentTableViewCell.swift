c3ed7774cd655f26bddb7da8f5fae51db9e6ae1a
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-02-19 16:38:07 +0100

    implemented content (post) uitableviewcell

--- HubchatTask/View/ContentTableViewCell.swift
+++ HubchatTask/View/ContentTableViewCell.swift
@@ -11,18 +11,104 @@ import UIKit
 class ContentTableViewCell: UITableViewCell {
 
     var avatarImage: UIImageView!
-
+    var contentTextLabel: UILabel!
+    var usernameLabel: UILabel!
+    var imagesView: UIView!
+    var firstImage: UIImageView!
+    var secondImage: UIImageView!
+    var thirdImage: UIImageView!
+    var fourthImage: UIImageView!
+    var moreImagesLabel: UILabel!
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.avatarImage = UIImageView()
+        self.contentTextLabel = UILabel()
+        self.usernameLabel = UILabel()
+        self.imagesView = UIView(frame: CGRect(x: 0, y: 0, width: 330, height: 70))
+        self.contentTextLabel.lineBreakMode = .byWordWrapping
+        self.contentTextLabel.numberOfLines = 5
+        self.contentTextLabel.font = UIFont.systemFont(ofSize: 12)
+
+        self.firstImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 65, height: 65))
+        self.secondImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 65, height: 65))
+        self.thirdImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 65, height: 65))
+        self.fourthImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 65, height: 65))
+        self.moreImagesLabel = UILabel()
+        self.moreImagesLabel.font = UIFont.boldSystemFont(ofSize: 18)
+        self.moreImagesLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         
         contentView.addSubview(avatarImage)
+        contentView.addSubview(contentTextLabel)
+        contentView.addSubview(usernameLabel)
+        imagesView.addSubview(firstImage)
+        imagesView.addSubview(secondImage)
+        imagesView.addSubview(thirdImage)
+        imagesView.addSubview(fourthImage)
+        imagesView.addSubview(moreImagesLabel)
+        contentView.addSubview(imagesView)
+
         
         self.avatarImage.snp.makeConstraints { (make) in
-            make.bottom.equalTo(contentView).inset(5)
+            make.bottom.equalTo(usernameLabel).inset(0)
             make.top.equalTo(contentView).inset(5)
             make.left.equalTo(contentView).inset(8)
             make.width.equalTo(90)
+            make.height.equalTo(90)
+        }
+        
+        self.contentTextLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(contentView).inset(0)
+            make.left.equalTo(avatarImage.snp.right).inset(-8)
+            make.width.equalTo(260)
+        }
+        
+        self.usernameLabel.snp.makeConstraints { (make) in
+            make.bottom.equalTo(avatarImage).inset(0)
+            make.left.equalTo(avatarImage.snp.right).inset(-8)
+            make.width.equalTo(260)
+        }
+        
+        self.imagesView.snp.makeConstraints { (make) in
+            make.top.equalTo(avatarImage.snp.bottom).inset(-5)
+            make.centerX.equalTo(contentView)
+            make.height.equalTo(69)
+            make.width.equalTo(310)
+        }
+        
+        self.firstImage.snp.makeConstraints { (make) in
+            make.left.equalTo(imagesView).inset(10)
+            make.right.equalTo(secondImage.snp.left).inset(-10)
+            make.bottom.equalTo(imagesView.snp.bottom).inset(2)
+            make.top.equalTo(imagesView).inset(2)
+            make.width.equalTo(65)
+            make.height.equalTo(65)
+        }
+        self.secondImage.snp.makeConstraints { (make) in
+            make.right.equalTo(thirdImage.snp.left).inset(-10)
+            make.bottom.equalTo(imagesView.snp.bottom).inset(2)
+            make.top.equalTo(imagesView).inset(2)
+            make.width.equalTo(65)
+            make.height.equalTo(65)
+        }
+        self.thirdImage.snp.makeConstraints { (make) in
+            make.right.equalTo(fourthImage.snp.left).inset(-10)
+            make.bottom.equalTo(imagesView.snp.bottom).inset(2)
+            make.top.equalTo(imagesView).inset(2)
+            make.width.equalTo(65)
+            make.height.equalTo(65)
+        }
+        self.fourthImage.snp.makeConstraints { (make) in
+            make.right.equalTo(imagesView.snp.right).inset(10)
+            make.bottom.equalTo(imagesView.snp.bottom).inset(2)
+            make.top.equalTo(imagesView).inset(2)
+            make.width.equalTo(65)
+            make.height.equalTo(65)
+        }
+        self.moreImagesLabel.snp.makeConstraints { (make) in
+            make.left.equalTo(fourthImage).inset(30)
+            make.top.equalTo(fourthImage).inset(10)
+            make.bottom.equalTo(fourthImage).inset(2)
         }
         
     }
@@ -30,11 +116,4 @@ class ContentTableViewCell: UITableViewCell {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
